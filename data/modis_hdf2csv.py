48c0d9d99449b740a4dc6a674b31f6324b0580af
Patricia Castellanos <p.castellanos@vu.nl>
2014-09-07 14:38:43 +0200

    download the hdf files

--- data/modis_hdf2csv.py
+++ data/modis_hdf2csv.py
@@ -1,19 +1,33 @@
 #!/bin/python
 # -*- coding: utf-8 -*-
-from datetime import date, timedelta as td
 
+import ftplib 
+import os.path
 if __name__ == "__main__":
     """ Download MODIS terra and aqua CMG active fire HDF files for a year 
         add them up and write to CSV """
         
-        
-    start = '201010'
+    ftp = ftplib.FTP('fuoco.geog.umd.edu')
+    ftp.login(user='fire',passwd='burnt')
+    ftp.cwd('modis/C5/cmg/monthly/hdf/')
+    start = '201001'
     end   = '201012'
 
     for y in range(int(start[0:4]),int(end[0:4])+1):
         for m in range(int(start[4:]),int(end[4:])+1):
-            print y,m
-            ftp://fuoco.geog.umd.edu/modis/C5/cmg/monthly/hdf/
+            
+            filename = 'MOD14CMH.'+str(y)+str(m).zfill(2) + '.005.01.hdf'
+            print filename
+            if not os.path.exists('hdf/'+filename):
+                localfile = open('hdf/'+filename, 'wb')
+                ftp.retrbinary('RETR ' + filename, localfile.write, 1024)
+            
+            filename = 'MYD14CMH.'+str(y)+str(m).zfill(2) + '.005.01.hdf'
+            print filename
+            if not os.path.exists('hdf/'+filename):
+                localfile = open('hdf/'+filename, 'wb')
+                ftp.retrbinary('RETR ' + filename, localfile.write, 1024)
+            
         
         
         
