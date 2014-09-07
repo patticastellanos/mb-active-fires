468bcd5189dea26ff24d3ad75030583963206b09
Patricia Castellanos <p.castellanos@vu.nl>
2014-09-07 17:33:21 +0200

    open and read files

--- data/modis_hdf2csv.py
+++ data/modis_hdf2csv.py
@@ -3,6 +3,9 @@
 
 import ftplib 
 import os.path
+from hdftools.openhdf import openhdf 
+import numpy as np
+
 if __name__ == "__main__":
     """ Download MODIS terra and aqua CMG active fire HDF files for a year 
         add them up and write to CSV """
@@ -13,21 +16,27 @@ if __name__ == "__main__":
     start = '201001'
     end   = '201012'
 
+    fires = np.zeros((360,720))
+    #add up active fires for a year
+    #download the data if needed
     for y in range(int(start[0:4]),int(end[0:4])+1):
         for m in range(int(start[4:]),int(end[4:])+1):
             
-            filename = 'MOD14CMH.'+str(y)+str(m).zfill(2) + '.005.01.hdf'
-            print filename
-            if not os.path.exists('hdf/'+filename):
-                localfile = open('hdf/'+filename, 'wb')
-                ftp.retrbinary('RETR ' + filename, localfile.write, 1024)
+            modfilename = 'MOD14CMH.'+str(y)+str(m).zfill(2) + '.005.01.hdf'
+            if not os.path.exists('hdf/'+modfilename):
+                localfile = open('hdf/'+modfilename, 'wb')
+                ftp.retrbinary('RETR ' + modfilename, localfile.write, 1024)
             
-            filename = 'MYD14CMH.'+str(y)+str(m).zfill(2) + '.005.01.hdf'
-            print filename
-            if not os.path.exists('hdf/'+filename):
-                localfile = open('hdf/'+filename, 'wb')
-                ftp.retrbinary('RETR ' + filename, localfile.write, 1024)
+            mydfilename = 'MYD14CMH.'+str(y)+str(m).zfill(2) + '.005.01.hdf'
+            if not os.path.exists('hdf/'+mydfilename):
+                localfile = open('hdf/'+mydfilename, 'wb')
+                ftp.retrbinary('RETR ' + mydfilename, localfile.write, 1024)
+                
+            # add up the active fires for the time range
+            print 'hdf/'+mydfilename
+            mydfile = openhdf('hdf/'+mydfilename)       
+            mydfires= mydfile.SD.select('CloudCorrFirePix')
+            fires   += mydfires.get()
             
-        
-        
+
         
